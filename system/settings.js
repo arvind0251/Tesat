9cb6ba01658af18cd4086f2c1fb3fe0d06800940
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-01-22 21:57:43 +0530

    Add files via upload

---
+++ system/settings.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+global.owner = "919149247203"
+global.footer = "BUG BOT MAD HINDU MOTU PATLU"
+global.idsal = "120363379684517003@newsletter"
+global.image = "https://files.catbox.moe/jp2qhx.jpg"
+global.linksal = "http://t.me/MOTU_PATALU_HINDU_HAI"
+global.autoread = false //true/false
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
